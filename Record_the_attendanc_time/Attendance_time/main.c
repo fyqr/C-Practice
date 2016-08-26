82105415730aec9c54554869ade5b5477df81a43
fyqr@163.com <ming123>
2016-08-26 10:17:40 +0800

    update

---
+++ Record_the_attendanc_time/Attendance_time/main.c
@@ -0,0 +1,11 @@
+//Record the attendance time of someone.
+//Because it's too late to someone's attendance!
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+    printf("Hello world!\n");
+    return 0;
+}
